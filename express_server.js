32e25c1d3f81c0824612434682cd1bd1c2339881
Miranda Zheng <zhengshuang2011@gmail.com>
2021-12-06 22:38:51 +0000

    learn how to build a web form and how to send the form data to a backend API that creates shortened URLs

--- express_server.js
+++ express_server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+const generateRandomString = () => {};
 
 app.set("view engine", "ejs");
 
@@ -28,6 +34,14 @@ app.get("/set", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
