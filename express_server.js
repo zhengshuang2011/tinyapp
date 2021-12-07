28ff4fcd64d4d1da9c7e685918ec4a4aca914253
Miranda Zheng <zhengshuang2011@gmail.com>
2021-12-07 02:13:53 +0000

    Redirect After Form Submission && Redirect Short URLs

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const request = require("request");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -10,7 +11,9 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
-const generateRandomString = () => {};
+const generateRandomString = () => {
+  return Math.random().toString(36).substring(2, 8);
+};
 
 app.set("view engine", "ejs");
 
@@ -26,11 +29,6 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -40,8 +38,29 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("OK");
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  // const array = Object.keys(urlDatabase);
+  // for (let key of array) {
+  //   if (urlDatabase[key] === longURL) {
+  //     res.redirect("/urls");
+  //   } else {
+  urlDatabase[shortURL] = longURL;
+  const pathName = `/urls/${shortURL}`;
+  res.redirect(pathName);
+  // }
+  // }
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  request(longURL, (error, response, body) => {
+    if (error) {
+      res.render("404");
+    } else if (response.statusCode === 200) {
+      res.redirect(longURL);
+    }
+  });
 });
 
 app.get("/urls/:shortURL", (req, res) => {
